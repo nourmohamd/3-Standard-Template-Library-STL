627dad8992d4c42ff9ad58c47aef9dab65d3b636
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-06-30 23:30:44 +0300

    Upload 01_Map_Of_STL_(How_To_Begin)

---
+++ 01_Map_Of_STL_(How_To_Begin)/01_Map_Of_STL_(How_To_Begin).cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Standard Template Library ---> STL
+    /*
+        Here Map For STL
+        ================
+        ================
+        The Guide To Finish All Concepts In STL
+        =======================================
+        Container <--------- Iterator -----------> Algorithm
+        =========            ========              =========
+           .                  It is                     swap()
+           .                  Pointer                   .
+           .                                            .
+           List                                         sort()
+           Vector                                       min()
+           Deque...                                     ...
+
+        Note:
+        =====
+        We Use Iterator For Working With Container For Create Algorithm
+
+        Type Of Container
+        ====Important====
+        Simple | Sequence | Associative | Unordered         | Adapter
+        =============================================================
+        Pair     Array      Map           Unordered-Set       Stack
+                 Vector     MultiMap      Unordered-MultiSet  Queue
+                 List       Set           Unordered-Map       Priority-Queue
+                 Deque      MultiSet      Unordered-MultiMap
+                 Forward-List
+
+        Note:
+        =====
+        All Above Names They Are Containers But Deffinces With Them In Job
+
+        Note:
+        =====
+        I Use Containers For Problem Solving
+    */
+
+
+    return 0;
+    system("PAUSE");
+}
