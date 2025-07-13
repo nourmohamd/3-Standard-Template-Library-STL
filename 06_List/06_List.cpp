e6c566dc3699a9c610f87a00d47fbe317d04ccae
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-07-13 23:56:21 +0300

    Upload Introduction About 06_List

---
+++ 06_List/06_List.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // List From ( Sequense Container )
+    // ================================
+    /*
+        Why We Need The List Container
+        ==============================
+            The List Sequense Container Loves Because
+            It Have Two <<< Double Linked List >>>
+                            ==================
+            --> [ | ] --> [ | ] --> [ | ] <---
+                [ | ] <-- [ | ] <-- [ | ]
+            That Means Each Element In List Contains Two Pointers But There are Some Issues About That:
+            ===========================================================================================
+            1 - First Value|Element Have Two Pointer( P1 pointers To Head[First Value], P2 Pointer To Null Because )
+            2 - Last Value|Element Have One Pointer( P1 pointers To Null )
+    */
+
+
+
+
+    return 0;
+    system("PAUSE");
+}
