9ab6a7e0dd6104e38595132cafbe7cf102fee7b6
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-07-16 23:39:53 +0300

    Upload Introduction For 07_Forward_List

---
+++ 07_Forward_List/07_Forward_List.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <forward_list>
+using namespace std;
+int main() {
+    // Forward List
+    /*
+        Forward List Made Up From Single Linked List
+        ============================================
+
+        [head] --Next--> [] --Next--> [Tail] --Null-->
+
+        Fast In Add & Delete & Safe Storage More Than List
+        And Has <<<(One)>>> Pointer From Back To Front Only
+                ===========
+
+        - Need Package: #include<forward_list>
+    */
+
+
+
+
+    return 0;
+    system("PAUSE");
+}
