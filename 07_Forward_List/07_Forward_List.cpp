cb3e95eb13f6f5c75270771b35ee071fc9fc216b
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-07-17 18:39:09 +0300

    Update 06_List.png Photo ------And------ Update 07_Forward_List.png Photo -------And-------- Type Some Methods For Forward List

--- 07_Forward_List/07_Forward_List.cpp
+++ 07_Forward_List/07_Forward_List.cpp
@@ -14,7 +14,39 @@ int main() {
                 ===========
 
         - Need Package: #include<forward_list>
+
+        Forward_List's Methods
+        ======================
+        1 - fl.assign(num1, num2) ===> For Reserve num1 Boxes In List With num2 Values
+        2 - 2 2Ways To Print By ForEach
     */
+    // Ex1:
+    forward_list<int> fl1;
+    fl1.assign(2, 100);
+    // 100 100
+    fl1.assign({1, 2, 3});
+    // 1 2 3
+    for(auto i: fl1) {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    // Output: 1 2 3
+
+    // Ex2: Print by foreach
+    // way1
+    for(auto i: fl1) {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    // Output: 1 2 3
+    // way2
+    // By Datatype forward list ===> int
+    for(int&i:fl1) {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    // Output: 1 2 3
+
 
 
 
