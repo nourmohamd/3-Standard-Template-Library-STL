c5a1e4bcf2a8339877198dbc82a2515a7bba1e39
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-07-29 20:15:38 +0300

    Upload 09_Set Section 2 in Set

--- 09_Set/09_Set.cpp
+++ 09_Set/09_Set.cpp
@@ -50,7 +50,21 @@ int main() {
         6- s.emplace_hint() ===> For Add But It Recives Value, Position_of_Add
                                  But in string Sorted By First Letter And Return
                                  This Position
-
+        Method In Set Section 2:
+        ========================
+        1- s.erase("nour") ===> Delete nour in Set
+        2- s.find(Value) ===> For Search About Any Value In Set
+        3- s.lower_bound(Value) ===> Pointers To Lower Value in SET
+        4- s.upper_bound(Value) ===> Pointers To Upper Value in SET
+        5- We Use s.lower_bound(Value) & s.upper_bound(Value) For Remove Range
+        6- s.swap(s2) ===> For Swap Between Two Set Object
+        7- s.clear()
+        8- s.key_comp() ===> For Compare Between Values In Set
+        9- s.count(num) ===> For Check If Number Sended Has Find In Set(1) Or No(0)
+        10- s.equal_range(2) ===> Recives Number And Return This Number(2) And Next Number Greater Than From 2
+                                  Need Iterator
+        Note: In Set You Cann't Update Or Change Values Of Boxes
+        11- set<int, greater<int>> ===> For Sorted Set Descending
     */
     // Ex1:
     set<int> s1 = {1, 2, 3, 4};
@@ -126,7 +140,76 @@ int main() {
         // ahmad
     */
 
+    // Ex5:
+    set<int> s3 = {1, 2, 3, 4};
+    auto it3 = s3.begin();
+    s3.erase(it3);
 
+    for(auto i:s3) {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    // Output: 2 3 4
+    it3++++;
+    // Every ++ Means Move For One Box Forward In Set
+    s3.erase(it3);
+
+    for(auto i:s3) {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    // Output: 3 4
+
+    // Ex6:
+    s3 = {1, 2, 3, 4, 5};
+    auto it4 = s3.find(2);
+    s3.erase(it4, s3.end());
+    for(auto i:s3) {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    // Output: 1
+    // Because It Remove From 2 To End : s3.erase(it4, s3.end())
+
+    // Ex7: Remove Range
+    set<int> s4 = {10, 20, 30, 40, 50};
+    auto it5 = s4.lower_bound(20);
+    auto it6 = s4.upper_bound(50);
+    s4.erase(it5, it6);
+    for(auto i:s4) {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    // Ouput: 10
+
+    // Ex8: key_comp()
+    set<int> s5 = {10, 20, 30, 40, 50, 60};
+    auto t0 = s5.key_comp();
+    auto t1 = s5.begin();
+    int n = 30;
+    for(;t0(*t1, n);t1++) {
+        cout<<*t1<<" ";
+    }
+    cout<<endl;
+    // Output: 10 20
+
+    // Ex9: count
+    cout<<s5.count(10)<<endl;
+    // Output: 1
+
+    // Ex10: equal_range
+    set<int> s6 = {1, 2, 3};
+    auto it7 = s6.equal_range(2);
+    cout<<*it7.first<<" "<<*it7.second<<endl;
+    // Output: 2 3
+
+    // Ex11:
+    set<int, greater<int>> s7 = {10, 20, 30};
+    for(auto i:s7) {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    // Output: 30 20 10
 
 
 
