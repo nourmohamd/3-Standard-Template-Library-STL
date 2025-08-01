5fce9e54b5f590fa1098745206629870db101118
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-08-01 04:00:18 +0300

    Update 011_Map Section 1

--- 011_Map/011_Map.cpp
+++ 011_Map/011_Map.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <iterator>
 using namespace std;
 int main() {
     // Map Container From ( Associative Container )
@@ -62,6 +63,42 @@ int main() {
         -const_reverse_iterator
     */
 
+    // Ex2: Insert Method
+
+    // Form1:
+    m.insert({12322, "AA"});
+
+    // Form2:
+    m.insert(pair<int, string>(2, "AHJJ"));
+
+    // Form3:
+    m.insert(make_pair(12331, "JJJJJJJ"));
+
+    // Form4:
+    auto it2 = m.begin();
+    m.insert(it2, {22,"AA"});
+
+    // Form5:
+    auto it3 = m.begin();
+    it3 = m.insert(it3, {101, "101"});
+    m.insert({102, "102"});
+    // Here We Storage Iterator And Position Of Operation In Iterator
+
+    // Form6:
+    map<char, int> m2 {{'a', 10}, {'b', 20}, {'c', 30}};
+    m2.insert({'d', 40});
+    auto it4 = m2.begin();
+    it4 = m2.insert(it4, {'e', 50});
+    m2.insert(it4, {'f', 60});
+    // Move From a to f ===> from m to m2;
+    map<char, int> m22;
+    m22.insert(m2.begin(), m2.find('f'));
+    // Not Including End Stop At f(Not Include)
+    for(auto i:m22) {
+        cout<<i.first<<" "<<i.second<<" - ";
+    }
+    cout<<endl;
+    // Output: a 10 - b 20 - c 30 - d 40 - e 50 -
 
 
 
