a3faf0198f3cdd7717532de6832934a2a1d77088
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-08-18 22:08:42 +0300

    Upload 014_Unordered_Types

---
+++ 014_Unordered_Types/014_Unordered_Types.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Unordered Types
+    // From Container ( Unordered Associative Containers )
+    /*
+        Properties For This:
+        ====================
+        Add|Delete|Search ===> O(1)
+
+        Fast In Add|Delete|Search From Previous Containers:
+        ===================================================
+        vector deque list forward_list set map multiset multimap pair
+
+        Types Of Unordered Associative Containers:
+        ==========================================
+        1- unordered_set
+        2- unordered_map
+        3- unordered_multiset
+        4- unordered_multimap
+
+        How It Work:
+        ============
+        It Depend On Buckets And Single Linked List And Hash Function
+        See Image For Understand What i mean?
+        <<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>
+    */
+
+
+
+
+    return 0;
+    system("PAUSE");
+}
