d3b82209f2f22630464001042910e24fca5ba88e
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-08-29 21:07:45 +0300

    Upload 018_Unordered_MultiMap

---
+++ 018_Unordered_MultiMap/018_Unordered_MultiMap.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+int main() {
+    // Unordered_MultiMap From ( Unordered Associative Container )
+    /*
+        1- ADD&DELETE&SEARCH ===> O(1)
+        2- Key Cannot Changed
+        3- Support Random Access
+        4- Elements Is Not Sorted
+        5- Repeated Elements
+    */
+    unordered_multimap<string, int> um;
+
+
+
+
+
+    return 0;
+    system("PAUSE");
+}
